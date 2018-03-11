dd6d3bc4d080794e9ce17a9ca4f37f5fbf283a20
Zheng Xiang <xiang.zheng@linaro.org>
2018-03-12 00:06:13 +0800

    update vm_loop_test to boot multi VMS

--- qemu/tests/vm_loop_test.py
+++ qemu/tests/vm_loop_test.py
@@ -4,7 +4,7 @@ from autotest.client.shared import error
 from autotest.client import utils
 from avocado.utils.git import get_repo
 
-from virttest import utils_test, env_process
+from virttest import utils_test, env_process, data_dir
 from virttest.staging import utils_memory
 
 
@@ -23,12 +23,9 @@ def run(test, params, env):
     def vm_loop_test(session, vm_name):
         vm = env.get_vm(vm_name)
         ip = vm.get_address()
-        git_url = params.get("git_url")
-        dest_dir = params.get("dest_dir")
-        get_repo(git_url, destination_dir=dest_dir)
 
-        hackbanch_groups = params.get("hackbanch_groups", "500")
-        hackbanch_loops = params.get("hackbanch_loops", "100")
+        hackbanch_groups = params.get("hackbench_groups", "500")
+        hackbanch_loops = params.get("hackbench_loops", "100")
         host_path = dest_dir + "/hackbench.c"
         guest_path = "/root/hackbench.c"
         try:
@@ -39,24 +36,24 @@ def run(test, params, env):
             status, output = session.cmd_status_output("md5sum %s" % guest_path, timeout=60)
             if status != 0:
                 logging.error(output)
-                raise error.TestFail("Fail to copy %s to guest %s" % (host_path, guest_path))
+                raise error.TestFail("%s: Fail to copy %s to guest %s" % (vm_name, host_path, guest_path))
             else:
-                logging.info("MD5 guest: %s" % output)
+                logging.info("%s: MD5 %s" % (vm_name, output))
             
             status, output = session.cmd_status_output("gcc -o /root/hackbench -pthread %s" % guest_path, timeout=60)
             if status != 0:
                 logging.error(output)
-                raise error.TestFail("Fail to compile %s" % guest_path)
+                raise error.TestFail("%s: Fail to compile %s" % (vm_name, guest_path))
             
             status, output = session.cmd_status_output("./hackbench %s process %s" % (hackbanch_groups, hackbanch_loops), timeout=1200)
             if status != 0:
                 logging.error(output)
-                raise error.TestFail("Fail to run hackbench")
+                raise error.TestFail("%s: Fail to run hackbench" % vm_name)
 
             status, output = session.cmd_status_output("cyclictest -t `nproc` -l 10000", timeout=1200)
             if status != 0:
                 logging.error(output)
-                raise error.TestFail("Fail to run cyclictest")
+                raise error.TestFail("%s: Fail to run cyclictest" % vm_name)
         finally:
             status, _ = utils_test.ping(ip, count=10, timeout=30)
             if status != 0:
@@ -69,8 +66,21 @@ def run(test, params, env):
                      " set up guest number to 1")
         guest_number = 1
 
+    image_name = params.get("image_name", "")
+    image_format = params.get("image_format", "qcow2")
+    image_clone_command = params.get("image_clone_command", "cp %s %s")
+    image_path = data_dir.get_data_dir() + "/" + image_name
     for tag in range(1, guest_number):
-        params["vms"] += " guest_%s" % tag
+        vm_name = "guest%s" % tag
+        params["vms"] += " %s" % vm_name
+        image_id = "image%s" % (tag + 1)
+        params["images_%s" % vm_name] = image_id
+        image_name_tag = "%s_%s" % (image_name, tag)
+        params["image_name_%s_%s" % (image_id, vm_name)] = image_name_tag
+        output = utils.system_output(image_clone_command % (image_path + "." + image_format,
+                                                            image_path + ("_%s." % tag) + image_format))
+        output = utils.system_output(image_clone_command % (image_path + "_AAVMF_VARS.fd",
+                                                            image_path + ("_%s_AAVMF_VARS.fd" % tag)))
 
     params["start_vm"] = "yes"
     login_timeout = int(params.get("login_timeout", 360))
@@ -86,6 +96,10 @@ def run(test, params, env):
             raise error.TestFail("Could not log into guest %s" % vm_name)
 
         sessions_info.append([session, vm_name])
+
+    git_url = params.get("git_url")
+    dest_dir = params.get("dest_dir")
+    get_repo(git_url, destination_dir=dest_dir)
 
     # run vm loop test in vms
     try:
